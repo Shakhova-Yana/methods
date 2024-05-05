c05ad36e74f85dd44b866039f39e0626d0a4a31e
НафНафДурикович <yana88yana99@gmail.com>
2024-05-06 01:27:45 +0300

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,46 @@
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+//task1
         int year = new Scanner(System.in).nextInt();
         checkYearIsLeapAndPrint(year);
+        System.out.println();
+//task2
+
+        int clientOS = 1;
+        int clientDeviceYear = 2015;
+       // String a = validateDeviceAndYear(clientOS, clientDeviceYear);
+       //String b = validateDeviceAndYear(2, clientDeviceYear);
+       // String c = validateDeviceAndYear(1, 2015);
+        System.out.println(validateDeviceAndYear(clientDeviceYear));
     }
 
-    public static void checkYearIsLeapAndPrint(int leapYear) {
-        boolean leapYearConditions = (leapYear % 400 == 0) || (leapYear % 4 == 0 && leapYear % 100 != 0);
-        boolean validYear = leapYear >= 1584;
-        if (validYear && leapYearConditions) {
-            System.out.printf("%s год - високосный год", leapYear);
+    //task2
+    public static String validateDeviceAndYear(int deviceType, int year) {
+        int currentYear = LocalDate.now().getYear();
+        if (deviceType == 0 && year < currentYear) {
+            return "Установите облегченную версию приложение для iOS по ссылке";
+        } else if (deviceType == 0) {
+            return "Установите версию приложение для iOS по ссылке";
+        } else if (deviceType == 1 && year < currentYear) {
+            return "Установите облегченную версию приложение для Android по ссылке";
+        } else if (deviceType == 1) {
+            return "Установите версию приложение для Android по ссылке";
         } else {
-            System.out.printf("%s год - не високосный год", leapYear);
+            return "Такая версия не поддерживается";
+        }
+    }
+
+    //task1
+    public static void checkYearIsLeapAndPrint(int yearInsideThisMethod) {
+        boolean leapYearConditions = (yearInsideThisMethod % 400 == 0) || (yearInsideThisMethod % 4 == 0 && yearInsideThisMethod % 100 != 0);
+        boolean validYear = yearInsideThisMethod >= 1584;
+        if (validYear && leapYearConditions) {
+            System.out.printf("%s год - високосный год", yearInsideThisMethod);
+        } else {
+            System.out.printf("%s год - не високосный год", yearInsideThisMethod);
         }
     }
 }
