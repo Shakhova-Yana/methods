4393079ac3681368df1e85d4af2e2a34fee0828e
НафНафДурикович <yana88yana99@gmail.com>
2024-05-06 00:13:10 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        int year = new Scanner(System.in).nextInt();
+        checkYearIsLeapAndPrint(year);
+    }
 
+    public static void checkYearIsLeapAndPrint(int leapYear) {
+        boolean leapYearConditions = (leapYear % 400 == 0) || (leapYear % 4 == 0 && leapYear % 100 != 0);
+        boolean validYear = leapYear >= 1584;
+        if (validYear && leapYearConditions) {
+            System.out.printf("%s год - високосный год", leapYear);
+        } else {
+            System.out.printf("%s год - не високосный год", leapYear);
+        }
     }
 }
