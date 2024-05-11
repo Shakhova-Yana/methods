8253c7faa6a21bc0f5091298fab03e269a2bac88
НафНафДурикович <yana88yana99@gmail.com>
2024-05-11 23:43:46 +0300

    Задание 3

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,35 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 //task1
-        int year = new Scanner(System.in).nextInt();
-        checkYearIsLeapAndPrint(year);
-        System.out.println();
+        {
+            System.out.println("\nTask 1");
+
+            int year = new Scanner(System.in).nextInt();
+            checkYearIsLeapAndPrint(year);
+            System.out.println();
+        }
 
 //task2
-        int clientDeviceYear = 2015;
-        String  deviceAndYear = validateDeviceAndYear(1, clientDeviceYear);
-        System.out.println(deviceAndYear);
+        {
+            System.out.println("\nTask 2");
+
+            int clientDeviceYear = 2015;
+            String deviceAndYear = validateDeviceAndYear(1, clientDeviceYear);
+            System.out.println(deviceAndYear);
+        }
+
+//task3
+        {
+            System.out.println("\nTask 3");
+
+            int deliveryDistance = 95;
+            String deliveryTime = calculateDeliveryDistance(deliveryDistance);
+
+            System.out.printf("Доставка в пределах %s км потребует дней: %s",deliveryDistance, deliveryTime );
+        }
     }
 
-//task1
+    //task1
     public static void checkYearIsLeapAndPrint(int yearInsideThisMethod) {
         boolean leapYearConditions = (yearInsideThisMethod % 400 == 0) || (yearInsideThisMethod % 4 == 0 && yearInsideThisMethod % 100 != 0);
         boolean validYear = yearInsideThisMethod >= 1584;
@@ -25,7 +43,7 @@ public class Main {
         }
     }
 
-//task2
+    //task2
     public static String validateDeviceAndYear(int deviceType, int year) {
         int currentYear = LocalDate.now().getYear();
         if (deviceType == 0 && year < currentYear) {
@@ -38,6 +56,15 @@ public class Main {
             return "Установите версию приложение для Android по ссылке";
         } else {
             return "Такая версия не поддерживается";
+        }
+    }
+
+    //task3
+    public static String calculateDeliveryDistance(int distance) {
+        if (distance > 100) {
+            return "Доставки нет";
+        } else {
+            return ((int) Math.ceil((double) (distance - 20) / 40) + 1) + "";
         }
     }
 }
